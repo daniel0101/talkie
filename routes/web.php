cd924e972ff8211599c65b2dd2b1315d68b69075
Awujoola <danielawujoola1@gmail.com>
2019-03-20 01:08:09 +0100

    route name change

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads','ThreadsController@index');
-Route::get('/threads/create-thread','ThreadsController@create')
+Route::get('/threads/create','ThreadsController@create')
 ;
 Route::get('/threads/{channel}','ThreadsController@index');
 Route::post('/threads','ThreadsController@store');
