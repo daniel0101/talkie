ed4f9ecc469561b729356f0608f95b2d8a6be45a
Awujoola <danielawujoola1@gmail.com>
2019-03-20 16:57:22 +0100

    use View share

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('threads.create',function($view){
-            $view->with('channels',Channel::all());
-        });
+        // View::composer('threads.create',function($view){
+        //     $view->with('channels',Channel::all());
+        // });
+        View::share('channels',Channel::all());
     }
 
     /**
