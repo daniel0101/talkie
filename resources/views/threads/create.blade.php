0234047e9baefcd26c159a54f2645f8fff1d8e3e
Awujoola <danielawujoola1@gmail.com>
2019-03-20 16:53:39 +0100

    add channels id select

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -9,17 +9,33 @@
 
                 <div class="card-body">
                     <form action="/threads" method="post">
-                        {{csrf_field()}}
+                       @csrf
+                        <div class="form-group">
+                            <label for="channel_id">Choose a category</label>
+                            <select name="channel_id" class="form-control" required>
+                                <option value="">Choose One</option>
+                                @foreach ($channels as $channel)
+                                <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                @endforeach 
+                            </select>
+                        </div>
                         <div class="form-group">
                             <label for="name">Title</label>
-                            <input type="text" name="title" class="form-control" placeholder=""> 
+                            <input type="text" name="title" class="form-control" placeholder="" value="{{ old('title') }}" required> 
                         </div>
                         <div class="form-group">
                             <label for="description">Body</label>
-                            <textarea name="body" rows="5" class="form-control"></textarea>
+                            <textarea name="body" rows="5" class="form-control" required>{{ old('body') }}</textarea>
                         </div>
                         <button type="submit" class="btn btn-fill btn-info">Post</button>
                     </form>
+                    @if(count($errors))
+                        <ul class="alert alert-danger">
+                        @foreach ($errors->all() as $error)                            
+                            <li>{{ $error }}</li>
+                        @endforeach
+                        </ul>
+                    @endif
                 </div>
             </div>
         </div>
