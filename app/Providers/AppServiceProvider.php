df4e3fda6d19ac991a318d673c83d9c4befc8e9b
Awujoola <danielawujoola1@gmail.com>
2019-03-20 16:53:04 +0100

    Add View composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use View;
+use App\Channel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('threads.create',function($view){
+            $view->with('channels',Channel::all());
+        });
     }
 
     /**
